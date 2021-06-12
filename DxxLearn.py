ac77cd886eba89e0f6d853986a027f7f11c53b14
之雨商 <1299172402@qq.com>
2021-06-12 11:50:40 +0800

    Update DxxLearn.py

--- DxxLearn.py
+++ DxxLearn.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import os
-# import time
 
 Cookie = os.environ["cookies"]
 
@@ -12,43 +11,12 @@ headers = {
     "Referer": "https://m.bjyouth.net/qndxx/index.html"
 }
 
-def GetIndex():
-    url = "https://m.bjyouth.net/dxx/index"
-    return json.loads(requests.get(url, headers=headers).text)
-
-def GetMyInfo():
-    url = "https://m.bjyouth.net/dxx/my"
-    return json.loads(requests.get(url, headers=headers).text)
-
 def AchieveDXX():
     url = "https://m.bjyouth.net/dxx/check?id=45&org_id=3574566"
     return requests.get(url, headers=headers)
 
-def GetMyLearned():
-    url = "https://m.bjyouth.net/dxx/my-integral"
-    return json.loads(requests.get(url, headers=headers).text)
-
-'''
-def CheckCourse():
-    res = GetIndex()
-    title_new = res["newCourse"]["title"]
-
-    res = GetMyLearned()
-    title_latest = res["data"][0]["title"]
-
-    if title_latest!=title_new:
-        print(f"未学习最新一期")
-        raise ValueError("NOT Learned")
-    else:
-        print(f"最新的 {title_latest} 已完成")
-'''
-
 def main():
-    print(GetIndex())
     AchieveDXX()
-    # time.sleep(10)
-    # print(GetMyLearned())
-    # CheckCourse()
 
 if __name__ == '__main__':
     main()
